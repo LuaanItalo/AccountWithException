8d849f5cdadc0e21eed4452d7a080aeb57608672
Luan Italo <luanitalo4545@gmail.com>
2024-08-25 00:53:05 -0300

    First commit

---
+++ AccountWithException/Entities/Account.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using ContaComException.Entities.Exceptions;
+
+namespace ContaComException.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+
+        public string Holder { get; set; }
+
+        public double Balance { get; protected set; }
+
+        public double WithDrawLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        void Deposit(double amount)
+        {
+           
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+
+
+            ExceedLimit(amount);
+            InsufficientBalance(amount);
+            Balance -= amount;
+            DisplayInfo();
+        }
+
+        private void InsufficientBalance(double amount)
+        {
+            if (Balance < amount)
+            {
+                throw new DomainException("Insufficient account balance");
+            }
+
+        }
+
+        private void ExceedLimit(double amount)
+        {
+
+            if (WithDrawLimit < amount)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"New Balance: {Balance:C}");
+        }
+    }
+}
---
+++ AccountWithException/Program.cs
@@ -0,0 +1,35 @@
+﻿using ContaComException.Entities.Exceptions;
+using ContaComException.Entities;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter sccount data: ");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine()!);
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine()!;
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withDrawLimit = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, initialBalance, withDrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            account.Withdraw(amount);
+           
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Withdraw error: {e.Message}");
+        }
+    }
+}
